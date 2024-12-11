4e33e5743110c1ebd1851122e90838c07809298d
Serhii.Bondar <serhii.bondar@codeminers.com.ua>
2024-12-11 15:28:55 +0200

    Initial commit

---
+++ src/main/java/ua/comparus/getuserstesttask/GetUsersTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ua.comparus.getuserstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetUsersTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetUsersTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/comparus/getuserstesttask/GetUsersTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.comparus.getuserstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetUsersTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
